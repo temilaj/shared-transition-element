bb651f2d473a233b9eaf9f15744ba8a6f33993d4
temilaj <temilaj@hotmail.com>
2021-12-29 00:29:40 -0500

    improve shared transition between elements

--- SharedElementTransitions/ContentView.swift
+++ SharedElementTransitions/ContentView.swift
@@ -14,46 +14,114 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             if !show {
-                VStack(alignment: .leading, spacing: 12) {
-                    Text("Hidden Text")
-                        .font(.largeTitle.weight(.bold))
-                        .matchedGeometryEffect(id: "title", in: namespace)
-                    .frame(maxWidth: .infinity, alignment: .leading)
+                VStack {
                     
-                    Text("20 sections - 3 hours".uppercased())
-                        .font(.footnote.weight(.semibold))
-                        .matchedGeometryEffect(id: "subtitle", in: namespace)
+                    Spacer()
+                    VStack(alignment: .leading, spacing: 12) {
+                        Text("Header Text")
+                            .font(.largeTitle.weight(.bold))
+                            .matchedGeometryEffect(id: "title", in: namespace)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        
+                        Text("20 sections - 3 hours".uppercased())
+                            .font(.footnote.weight(.semibold))
+                            .matchedGeometryEffect(id: "subtitle", in: namespace)
+                        
+                        Text("iPhone app for iOS 15 with custom layouts, animations, transitions, and ...")
+                            .font(.footnote)
+                            .matchedGeometryEffect(id: "description", in: namespace)
+                    }
+                    .padding(20)
+                    .background(
+                        Rectangle()
+                            .fill(.ultraThinMaterial)
+                            .mask(RoundedRectangle(cornerRadius: 30, style: .continuous))
+                            .blur(radius: 30)
+                            .matchedGeometryEffect(id: "blur", in: namespace)
+                    )
                     
-                    Text("iPhone app for iOS 15 with custom layouts, animations, transitions, and ...")
-                        .font(.footnote)
-                        .matchedGeometryEffect(id: "description", in: namespace)
                 }
-                .padding(20)
                 .foregroundStyle(.white)
                 .background(
-                    Color.red.matchedGeometryEffect(id: "background", in: namespace)
+                    Image("Illustration")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .matchedGeometryEffect(id: "image", in: namespace)
                 )
+                .background(
+                    Image("BackgroundImage")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .matchedGeometryEffect(id: "background", in: namespace)
+                )
+                .mask(RoundedRectangle(cornerRadius: 30, style: .continuous)
+                        .matchedGeometryEffect(id: "mask", in: namespace)
+                )
+                .frame(height: 300)
                 .padding(20)
             } else {
-                VStack(alignment: .leading, spacing: 12) {
-                    Spacer()
-                    Text("iPhone app for iOS 15 with custom layouts, animations, transitions, and ...")
-                        .font(.footnote)
-                        .matchedGeometryEffect(id: "description", in: namespace)
-                    Text("20 sections - 3 hours".uppercased())
-                        .font(.footnote.weight(.semibold))
-                        .matchedGeometryEffect(id: "subtitle", in: namespace)
-                    
-                    Text("Shown Text")
-                        .font(.largeTitle.weight(.bold))
-                        .matchedGeometryEffect(id: "title", in: namespace)
-                    .frame(maxWidth: .infinity, alignment: .leading)
+                ScrollView {
+                    VStack {
+                        Spacer()
+                    }
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 500)
+                    .foregroundStyle(.black)
+                    .background(
+                        Image("Illustration")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .matchedGeometryEffect(id: "image", in: namespace)
+                    )
+                    .background(
+                        Image("BackgroundImage")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .matchedGeometryEffect(id: "background", in: namespace)
+                    )
+                    .mask(RoundedRectangle(cornerRadius: 30, style: .continuous)
+                        .matchedGeometryEffect(id: "mask", in: namespace)
+                    )
+                    .overlay {
+                        VStack(alignment: .leading, spacing: 12) {
+                            Text("Header Text")
+                                .font(.largeTitle.weight(.bold))
+                                .matchedGeometryEffect(id: "title", in: namespace)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                            
+                            Text("20 sections - 3 hours".uppercased())
+                                .font(.footnote.weight(.semibold))
+                                .matchedGeometryEffect(id: "subtitle", in: namespace)
+                            
+                            
+                            Text("iPhone app for iOS 15 with custom layouts, animations, transitions, and ...")
+                                .font(.footnote)
+                                .matchedGeometryEffect(id: "description", in: namespace)
+                            
+                            Divider()
+                            HStack {
+                                Image("profile")
+                                    .resizable()
+                                    .frame(width: 26, height: 26)
+                                    .cornerRadius(10)
+                                    .padding(8)
+                                    .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 18, style: .continuous))
+                                
+                                Text("Styled by Temi")
+                                    .font(.footnote)
+                            }
+                        }
+                        .padding(20)
+                        .background(
+                            Rectangle()
+                                .fill(.ultraThinMaterial)
+                                .mask(RoundedRectangle(cornerRadius: 30, style: .continuous))
+                                .matchedGeometryEffect(id: "blur", in: namespace)
+                        )
+                        .offset(y: 250)
+                        .padding(20)
+                    }
                 }
-                .padding()
-                .foregroundStyle(.black)
-                .background(
-                    Color.blue.matchedGeometryEffect(id: "background", in: namespace)
-                )
             }
         }.onTapGesture {
             withAnimation(.spring(response: 0.5, dampingFraction: 0.8)) {
