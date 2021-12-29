8462781dd9c912ea26452aa06d94d1aa66351365
temilaj <temilaj@hotmail.com>
2021-12-28 21:38:18 -0500

    Initial Commit

---
+++ SharedElementTransitions/SharedElementTransitionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SharedElementTransitionsApp.swift
+//  SharedElementTransitions
+//
+//  Created by Temi Lajumoke on 12/28/21.
+//
+
+import SwiftUI
+
+@main
+struct SharedElementTransitionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
