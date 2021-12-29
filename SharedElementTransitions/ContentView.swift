cf86511e311eba3b4b47025a89bee55025290154
temilaj <temilaj@hotmail.com>
2021-12-28 21:59:47 -0500

    implement basic shared element transition

--- SharedElementTransitions/ContentView.swift
+++ SharedElementTransitions/ContentView.swift
@@ -8,9 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Namespace var namespace
+    @State var show = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            if !show {
+                VStack(alignment: .leading, spacing: 12) {
+                    Text("Hidden Text")
+                        .font(.largeTitle.weight(.bold))
+                        .matchedGeometryEffect(id: "title", in: namespace)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    
+                    Text("20 sections - 3 hours".uppercased())
+                        .font(.footnote.weight(.semibold))
+                        .matchedGeometryEffect(id: "subtitle", in: namespace)
+                    
+                    Text("iPhone app for iOS 15 with custom layouts, animations, transitions, and ...")
+                        .font(.footnote)
+                        .matchedGeometryEffect(id: "description", in: namespace)
+                }
+                .padding(20)
+                .foregroundStyle(.white)
+                .background(
+                    Color.red.matchedGeometryEffect(id: "background", in: namespace)
+                )
+                .padding(20)
+            } else {
+                VStack(alignment: .leading, spacing: 12) {
+                    Spacer()
+                    Text("iPhone app for iOS 15 with custom layouts, animations, transitions, and ...")
+                        .font(.footnote)
+                        .matchedGeometryEffect(id: "description", in: namespace)
+                    Text("20 sections - 3 hours".uppercased())
+                        .font(.footnote.weight(.semibold))
+                        .matchedGeometryEffect(id: "subtitle", in: namespace)
+                    
+                    Text("Shown Text")
+                        .font(.largeTitle.weight(.bold))
+                        .matchedGeometryEffect(id: "title", in: namespace)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                }
+                .padding()
+                .foregroundStyle(.black)
+                .background(
+                    Color.blue.matchedGeometryEffect(id: "background", in: namespace)
+                )
+            }
+        }.onTapGesture {
+            withAnimation(.spring(response: 0.5, dampingFraction: 0.8)) {
+                show.toggle()
+            }
+        }
     }
 }
 
